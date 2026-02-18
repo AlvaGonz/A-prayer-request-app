70fd912681e02a03f6155e33d04d20e48e03ccdc
AlvaGonz <Adrian.AAlvarezGonz@hotmail.com>
2026-02-17 21:53:09 -0400

    feat: add shareable prayer links - Share button on prayer cards for logged-in users - Public /shared/:token page for guests to pray and comment - Backend: shareToken on PrayerRequest, shared routes, guest comments - i18n: English and Spanish translations

--- prayer-board/src/App.jsx
+++ prayer-board/src/App.jsx
@@ -5,6 +5,7 @@ import { SocketProvider } from './context/SocketContext';
 import { ThemeProvider } from './context/ThemeContext';
 import ErrorBoundary from './components/ErrorBoundary';
 import PrayerWallPage from './pages/PrayerWallPage';
+import SharedPrayerPage from './pages/SharedPrayerPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import './styles/themes.css';
@@ -18,6 +19,7 @@ function App() {
             <Router>
               <Routes>
                 <Route path="/" element={<PrayerWallPage />} />
+                <Route path="/shared/:token" element={<SharedPrayerPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
               </Routes>
